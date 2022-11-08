08b1d91109914f60dcab813d56761f8e56208c7e
PierreC <pierre.clapisson@viacesi.fr>
2022-11-08 14:21:05 +0100

    tt js

--- groupe 3/script.js
+++ groupe 3/script.js
@@ -3,3 +3,21 @@ let P = document.getElementById("P");
 P.addEventListener('mouseover', (e) => {
     console.log("Bonjour");
 });
+
+let Y = document.getElementById("Y");
+
+Y.addEventListener('mouseover', (e) => {
+    console.log("Bonjour");
+});
+
+let E = document.getElementById("E");
+
+E.addEventListener('mouseover', (e) => {
+    console.log("Bonjour");
+});
+
+let A = document.getElementById("A");
+
+A.addEventListener('mouseover', (e) => {
+    console.log("Bonjour");
+});
