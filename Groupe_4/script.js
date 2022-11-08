a1ffa0da8da6f7537ff7dfced6f2f4daa3335419
acirieul <arthur.cirieul@viacesi.fr>
2022-11-08 14:29:10 +0100

    qdghj

--- Groupe_4/script.js
+++ Groupe_4/script.js
@@ -1,21 +1,7 @@
 let pAB = document.getElementById("pAB");
 
 pAB.addEventListener('mouseover', (e) => {
-    console.log("Antoine Bellet");
-});
-
-
-
-let p1 = document.getElementById("p1");
-
-p1.addEventListener('mouseover', (e)=>{
-    console.log("Bonjour c'est Evan");
-});
-
-
-
-let p32 = document.getElementById("p32");
-
+  
 p32.addEventListener('mouseover', (e)=>{
     console.log("Salut c'est Damien");
 });
