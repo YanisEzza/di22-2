c05af85ddf2f55603dd45857c8aadec51a6498e4
JulesW <jules.waghemacker@viacesi.fr>
2022-11-08 14:17:21 +0100

    zfefez

--- Groupe_4/script.js
+++ Groupe_4/script.js
@@ -9,3 +9,10 @@ let p32 = document.getElementById("p32");
 p32.addEventListener('mouseover', (e)=>{
     console.log("Salut c'est Damien");
 });
+
+
+let p2 = document.getElementById("p2");
+
+p1.addEventListener('mouseover', (e)=>{
+    console.log("salut c'est jules");
+});
